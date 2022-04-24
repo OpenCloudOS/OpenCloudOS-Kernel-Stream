801f14febdb8b862c7e6341a7bc0df3eb131764e
Fuhai Wang <fuhaiwang@tencent.com>
2022-04-24 19:49:54 +0800

    tcp: make TCP_RTO_MIN/MAX be tunable
    Upstream: no
    
    tcp_rto_min,tcp_rto_max control the parameters of them.
    remember that the min value of tcp_rto_min is 4ms which
    be used to be div by RTO_MAX.
    
    Signed-off-by: Shan Wei <davidshan@tencent.com>
    Signed-off-by: Fuhai Wang <fuhaiwang@tencent.com>
    Signed-off-by: katrinzhou <katrinzhou@tencent.com>
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -138,8 +138,9 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCP_DELACK_MIN	4U
 #define TCP_ATO_MIN	4U
 #endif
-#define TCP_RTO_MAX	((unsigned)(120*HZ))
-#define TCP_RTO_MIN	((unsigned)(HZ/5))
+
+#define TCP_RTO_MAX	((unsigned)(sysctl_tcp_rto_max*HZ))
+#define TCP_RTO_MIN	((unsigned)((sysctl_tcp_rto_min*HZ)/1000))
 #define TCP_TIMEOUT_MIN	(2U) /* Min timeout for TCP timers in jiffies */
 
 #define TCP_TIMEOUT_MIN_US (2*USEC_PER_MSEC) /* Min TCP timeout in microsecs */
@@ -259,6 +260,8 @@ extern int sysctl_tcp_init_cwnd;
 extern int sysctl_tcp_inherit_buffsize;
 extern int sysctl_tcp_init_rto;
 extern int sysctl_tcp_synack_rto_interval;
+extern int sysctl_tcp_rto_min;
+extern int sysctl_tcp_rto_max;
 
 #define TCP_RACK_LOSS_DETECTION  0x1 /* Use RACK to detect losses */
 #define TCP_RACK_STATIC_REO_WND  0x2 /* Use static RACK reo wnd */
