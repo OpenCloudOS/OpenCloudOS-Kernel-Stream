0d19f2d9342b2007ed1a82f34eefe67fc9aa776c
Kairui Song <kasong@tencent.com>
2022-06-21 16:23:28 +0800

    ttools: add ttools module to support ptrace protect
    Upstream status: downstream-only
    
    add ttools module to support ptrace protect and get
    file refcounts by fd
    
    Signed-off-by: Xiaoming Gao <newtongao@tencent.com>
    Signed-off-by: katrinzhou <katrinzhou@tencent.com>
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- include/linux/ptrace.h
+++ include/linux/ptrace.h
@@ -46,6 +46,7 @@ extern int ptrace_access_vm(struct task_struct *tsk, unsigned long addr,
 #define PT_EXITKILL		(PTRACE_O_EXITKILL << PT_OPT_FLAG_SHIFT)
 #define PT_SUSPEND_SECCOMP	(PTRACE_O_SUSPEND_SECCOMP << PT_OPT_FLAG_SHIFT)
 
+extern int (*ptrace_pre_hook)(long request, long pid, struct task_struct *task, long addr, long data);
 extern long arch_ptrace(struct task_struct *child, long request,
 			unsigned long addr, unsigned long data);
 extern int ptrace_readdata(struct task_struct *tsk, unsigned long src, char __user *dst, int len);
--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -1288,6 +1288,9 @@ int ptrace_request(struct task_struct *child, long request,
 #define arch_ptrace_attach(child)	do { } while (0)
 #endif
 
+int (*ptrace_pre_hook)(long request, long pid, struct task_struct *task, long addr, long data);
+EXPORT_SYMBOL(ptrace_pre_hook);
+
 SYSCALL_DEFINE4(ptrace, long, request, long, pid, unsigned long, addr,
 		unsigned long, data)
 {
@@ -1305,6 +1308,12 @@ SYSCALL_DEFINE4(ptrace, long, request, long, pid, unsigned long, addr,
 	if (!child) {
 		ret = -ESRCH;
 		goto out;
+	}
+
+	if (ptrace_pre_hook) {
+		ret = ptrace_pre_hook(request, pid, child, addr, data);
+		if (ret)
+			goto out_put_task_struct;
 	}
 
 	if (request == PTRACE_ATTACH || request == PTRACE_SEIZE) {
