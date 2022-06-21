23c0e267e6f94c504ebf09accd2fdfb65ecc7bbf
Kairui Song <kasong@tencent.com>
2022-06-21 16:21:59 +0800

    tcp: add tcp_loss_init_cwnd sysctl to tunnel packet numbers in loss
    Upstream status: downstream-only
    
    If RTO is timeout, we will endter tcp_enter_loss() and
    lower down cwnd to 1.
    
    Now we add a proc parameter to set the cwnd value in tcp_enter_loss.
    
    Signed-off-by: Shan Wei <davidshan@tencent.com>
    Signed-off-by: Samuel Liao <samuelliao@tencent.com>
    Signed-off-by: Xiaoming Gao <newtongao@tencent.com>
    Signed-off-by: Kairui Song <kasong@tencent.com>
    Signed-off-by: katrinzhou <katrinzhou@tencent.com>
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -82,6 +82,7 @@
 #include <net/mptcp.h>
 
 int sysctl_tcp_max_orphans __read_mostly = NR_FILE;
+int sysctl_tcp_loss_init_cwnd = 1;
 
 #define FLAG_DATA		0x01 /* Incoming frame contained data.		*/
 #define FLAG_WIN_UPDATE		0x02 /* Incoming ACK was a window update.	*/
@@ -2152,7 +2153,7 @@ void tcp_enter_loss(struct sock *sk)
 		tcp_ca_event(sk, CA_EVENT_LOSS);
 		tcp_init_undo(tp);
 	}
-	tcp_snd_cwnd_set(tp, tcp_packets_in_flight(tp) + 1);
+	tcp_snd_cwnd_set(tp, max((unsigned int)sysctl_tcp_loss_init_cwnd, tcp_packets_in_flight(tp) + 1));
 	tp->snd_cwnd_cnt   = 0;
 	tp->snd_cwnd_stamp = tcp_jiffies32;
 
