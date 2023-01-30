6c353a3bd06eae4133815c7cbea931261c546990
Kairui Song <kasong@tencent.com>
2023-01-30 13:53:52 +0800

    sched/psi: simplify cgroup psi retrieving
    Upstream: https://lore.kernel.org/lkml/20230208161654.99556-2-ryncsn@gmail.com/
    
    * This is part of EMM *
    
    Since the only case where cgroup_psi should return psi_system instead of
    the cgroup's psi_group is the root cgroup, just set root cgroup's psi to
    point to psi_system to remove the if branch.
    
    Signed-off-by: Kairui Song <kasong@tencent.com>
    Reviewed-by: Michal Koutný <mkoutny@suse.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -164,7 +164,12 @@ static struct static_key_true *cgroup_subsys_on_dfl_key[] = {
 static DEFINE_PER_CPU(struct cgroup_rstat_cpu, cgrp_dfl_root_rstat_cpu);
 
 /* the default hierarchy */
-struct cgroup_root cgrp_dfl_root = { .cgrp.rstat_cpu = &cgrp_dfl_root_rstat_cpu };
+struct cgroup_root cgrp_dfl_root = {
+	.cgrp.rstat_cpu = &cgrp_dfl_root_rstat_cpu,
+#ifdef CONFIG_PSI
+	.cgrp.psi = &psi_system,
+#endif
+};
 EXPORT_SYMBOL_GPL(cgrp_dfl_root);
 
 /*
